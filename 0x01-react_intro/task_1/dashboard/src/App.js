7b1be8729ebd2d700f5998b4c0449208d4468c98
Kazenzi <jacintakazenzi99@gmail.com>
2023-02-23 17:48:56 +0300

    mm

--- 0x01-react_intro/task_1/dashboard/src/App.js
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -1,10 +1,10 @@
 import logo from './holberton-logo.jpg';
 import './App.css';
+import { getFullYear, getFooterCopy } from "./utils";
 
 function App() {
   return (
     <div className="App">
-      
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
@@ -12,11 +12,11 @@ function App() {
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
-      <div className="App-footer">Copyright 2020 - holberton School</div>
+      <div className="App-footer">
+        Copyright {getFullYear()} - {getFooterCopy()}
+      </div>
     </div>
   );
 }
 
 export default App;
-
-
